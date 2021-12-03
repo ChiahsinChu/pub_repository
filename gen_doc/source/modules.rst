025b8c925c4eb4f9c27edf41909c8b63e73e7c69
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2021-12-04 00:21:49 +0800

    clean the dir

--- gen_doc/source/modules.rst
+++
@@ -1,7 +0,0 @@
-jxzhu_package
-=============
-
-.. toctree::
-   :maxdepth: 4
-
-   jxzhu_package
--- git.sh
+++
@@ -1,8 +0,0 @@
-# initialization
-git init
-# set origin as alias
-git remote add origin [https/ssh]
-git add .
-# you can also replace `.` with a specific dir or file
-git commit -m 'comments'
-git push -u origin master
